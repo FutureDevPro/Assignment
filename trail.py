80479ddecb31e87bc00e9edd7e4a2bee23c35b14
Udit Patel <144479971+FutureDevPro@users.noreply.github.com>
2024-02-24 18:36:43 +0530

    roy

--- trail.py
+++
@@ -1,9 +0,0 @@
-def sum_of_list(num):
-  return sum(int(i) for i in str(num))
-
-
-list1 = [987, 456, 345, 321, 654, 879, 346, 574, 859]
-sum_list= []
-sum_list = [sum_of_list(j) for j in list1]
-
-print(f"sum of the digits of numbers in a list : {sum_list}")
