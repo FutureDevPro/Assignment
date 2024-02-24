31967f77061bba1d53f3cfb6510cde29497a6d3c
Udit Patel <144479971+FutureDevPro@users.noreply.github.com>
2024-02-24 21:57:13 +0530

    new

---
+++ Python - Control Flow.py
@@ -0,0 +1,9 @@
+#1. Write a Python program to check if a given number is positive or negative.
+
+num = int(input("Enter Number : "))
+
+if num >= 0:
+    print(f"{num} is positive")
+
+else:
+    print(f"{num} is negetive")
